a97a440032bdfec4349698822e6dd8c7fcb477ee
Devyani Thokal <thokaldevyani123@gmail.com>
2026-07-16 11:54:23 +0530

    conditionals added

---
+++ 02_conditionals.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------
+# If Else Elif Statements
+# ------------------------------------------------------------------------
+
+if True:
+    print('true')
+
+if False:
+    print('false')
+else:
+    print('true')
+
+language = 'Python'
+
+if language >= 'Python':
+    print('Language is Python')
